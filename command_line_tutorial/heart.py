7d732eeba1af1bd09951ec059acc8adf372aeb9e
skuhn <skuhn@imm.cnrs.fr>
2026-02-20 11:07:54 +0100

    Added data sets

--- command_line_tutorial/heart.py
+++ command_line_tutorial/heart.py
@@ -1,13 +1,16 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Generate plotting values
 t = np.linspace(0, 2*np.pi, 200)
 x = 16 * np.sin(t)**3
 y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2 * np.cos(3*t) - np.cos(4*t)
 
+# Initialise figure
 fig, ax = plt.subplots()
+
+# define plot and text
 ax.plot(x, y, color='red', lw=3)
-ax.text(x=-3.5, y=0, s='Plinius Cursus')
+ax.text(x=-3.5, y=0, s='$Myxococcus xanthus$')
 
 plt.show()
