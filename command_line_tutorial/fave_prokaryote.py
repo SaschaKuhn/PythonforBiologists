4c48e6b386942139820d39a8ac901bcf0b969989
skuhn <57350351+SaschaKuhn@users.noreply.github.com>
2026-03-25 14:01:59 +0100

    Finches data. Fixed prokaryote plot

--- command_line_tutorial/fave_prokaryote.py
+++ command_line_tutorial/fave_prokaryote.py
@@ -7,7 +7,7 @@ x = 16 * np.sin(t)**3
 y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2 * np.cos(3*t) - np.cos(4*t)
 
 # Initialise figure
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(9,9))
 
 # define plot and text
 ax.plot(x, y, color='red', lw=3)
